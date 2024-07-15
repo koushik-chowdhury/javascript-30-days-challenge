5d8c745f819f812aee5e93434cecb8e96d81bb41
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-15 23:09:34 +0530

    variable and datatype

---
+++ day-1/index.js
@@ -0,0 +1,43 @@
+// Activity 1: variable declaration
+
+// task 1: declare a variable using var, assign a number, and log the value to the console.
+
+var num = 2002;
+console.log(num);
+
+// task 2: declare a variable using let, assign it a string, and log the value to the console.
+
+let Num = "My Name is Koushik Chowdhury";
+console.log(Num);
+
+// Activity 2: Constant Declaration
+
+// task 3: declare a variable using const, assign it a boolean value, and log the value  to the console.
+
+const bool = true;
+console.log(bool);
+
+// Activity 3: Data Types
+
+/* task 4: Create variables of diffrent data types (number, string, boolean, object, array) and log each 
+variables type using the typeof operator. */
+
+let number = 100;
+let str = "Javascript";
+let Bool = true;
+let Object = {
+    name: "koushik",
+    surname: "Chowdhury",
+    college: "Gour Mahavidyalaya"
+};
+let array = [1,2,3,4,5];
+let Null = null;
+let Undefined = undefined;
+console.log("\n-------------Datatypes-----------------\n");
+console.log(typeof number);
+console.log(typeof str);
+console.log(typeof Bool);
+console.log(typeof Object);
+console.log(typeof array);
+console.log(typeof Null);
+console.log(typeof Undefined);
