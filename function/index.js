8624cede78031f58629f066d6d2e41cc8b0fca28
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-21 23:44:30 +0530

    function

---
+++ function/index.js
@@ -0,0 +1,15 @@
+// Function
+
+function sayMyName() {
+  for (let i = 0; i < 10; i += 1) {
+    console.log("Koushik");
+  }
+}
+// sayMyName();
+ 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+let result = add(2, 4);
+console.log(result);
