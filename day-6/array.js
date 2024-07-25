41dd3a8742bc4a0de2e4d3d097c94487e81923cd
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-25 23:12:30 +0530

    array

---
+++ day-6/array.js
@@ -0,0 +1,14 @@
+// Arrays
+
+console.log(`------------Activity 1-----------`);
+
+// task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+
+// let array = new Array(1, 2, 3, 4, 5);
+
+let array = [1, 2, 3, 4, 5]
+console.log(array);
+
+// task 2: Access the first and last elements of the array and log them to the console.
+
+console.log(`First element of the array is ${array[0]} \nand Last element of the array is ${array[array.length - 1]}`);
---
+++ day-6/method.js
@@ -0,0 +1,15 @@
+// Activity 2
+
+console.log(`------------Activity 2-----------`);
+
+// task 3: Use the push method to add a new number to the end of the array and log the updated array 
+
+const array = new Array(1, 2, 3);
+
+// pushing
+array.push(6, 9);
+console.log(array);
+
+// popping
+array.pop();
+console.log(array);
