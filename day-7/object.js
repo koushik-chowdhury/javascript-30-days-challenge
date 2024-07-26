4533b9d9f0aa25d6353cfd92c4ef98c9d7854580
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-26 23:54:10 +0530

    object

---
+++ day-7/object.js
@@ -0,0 +1,12 @@
+// Objects
+
+console.log(`---------Activity 1--------`);
+// task 1: Create an object representing a book with properties like title, author, and year,
+
+const book = {
+  author: "Koushik Chowdhury",
+  title: "failure",
+  year: "2024",
+};
+console.log(book);
+console.log(`Title is ${book.title} and author is ${book.author}`);
