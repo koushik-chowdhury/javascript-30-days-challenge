559c1e3e6d5193f42743c91ff402ce5b9cfc6bad
Koushik Chowdhury <csakoushik@gmail.com>
2024-08-04 23:32:42 +0530

    background color changer

--- dom/index.js
+++ dom/index.js
@@ -1,10 +1,13 @@
 const Title = document.getElementById("title");
-const colors = ["red", "blue", "yellow"];
-const max = 2
-const min = 0
-const rand = Math.floor(Math.random() * (max - min + 1)) + min
-Title.style.backgroundColor = colors[rand];
+const colors = ["red", "blue", "yellow", "white", "violet", "grey"];
 
+function colorChange() {
+  const max = 5;
+  const min = 0;
+  const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+  Title.style.backgroundColor = colors[rand];
+  Title.style.transition = "0.5s all"
+}
 
-
+setInterval(colorChange, 1000);
 
